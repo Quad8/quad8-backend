c46a16c124dd5232d917af8ffe4b9ee3cb0297d0
Su-daa <subin.kk019@gmail.com>
2024-06-03 00:59:31 +0900

    Feat: User 엔티티, 레포지토리 추가

--- src/main/java/site/keydeuk/store/domain/user/repository/UserRepository.java
+++ src/main/java/site/keydeuk/store/domain/user/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package site.keydeuk.store.domain.user.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import site.keydeuk.store.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/site/keydeuk/store/entity/User.java
+++ src/main/java/site/keydeuk/store/entity/User.java
@@ -1,4 +1,40 @@
 package site.keydeuk.store.entity;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import site.keydeuk.store.common.entity.BaseTimeEntity;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private LocalDate birth;
+    private String phone;
+
+    //TODO: enum 클래스 생성 -> gender, role
+    private String gender;
+    private String role;
+    private boolean status;
+    private String nickname;
+
+    private String imgUrl;
+
+    private String provider;
+
+    private String providerId;
 }
