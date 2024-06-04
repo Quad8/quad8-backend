6a043c345be1e37f8f144d9da82afaee3cc31507
Su-daa <subin.kk019@gmail.com>
2024-06-04 19:56:41 +0900

    Feat: 자체 회원가입 controller 작성

--- src/main/java/site/keydeuk/store/domain/user/controller/UserController.java
+++ src/main/java/site/keydeuk/store/domain/user/controller/UserController.java
@@ -1,4 +1,24 @@
 package site.keydeuk.store.domain.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import site.keydeuk.store.common.response.CommonResponse;
+import site.keydeuk.store.domain.user.dto.JoinRequest;
+import site.keydeuk.store.domain.user.service.UserService;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public CommonResponse<Long> join(@RequestBody @Validated JoinRequest joinRequest) {
+        Long userId = userService.join(joinRequest);
+        return CommonResponse.ok(userId);
+    }
+
+
 }
