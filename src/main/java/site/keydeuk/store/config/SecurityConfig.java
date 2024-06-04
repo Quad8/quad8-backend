8471534e52df35b28a9d7f7e25ce1ac6c4786abf
Su-daa <subin.kk019@gmail.com>
2024-06-04 16:36:11 +0900

    Feat: succeddhandler 주석처리

--- src/main/java/site/keydeuk/store/config/SecurityConfig.java
+++ src/main/java/site/keydeuk/store/config/SecurityConfig.java
@@ -1,28 +1,32 @@
 package site.keydeuk.store.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import site.keydeuk.store.domain.user.service.OAuth2UserService;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-
+@Slf4j
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     private final OAuth2UserService oAuth2UserService;
 
-    public SecurityConfig(OAuth2UserService oAuth2UserService) {
-        this.oAuth2UserService = oAuth2UserService;
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() { // security를 적용하지 않을 리소스
+        return web -> web.ignoring()
+                // error endpoint를 열어줘야 함, favicon.ico 추가!
+                .requestMatchers("/error", "/favicon.ico");
     }
 
     @Bean
@@ -31,7 +35,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("/localhost:8080/**")
                         .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                         .allowCredentials(true)
@@ -42,32 +46,33 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(config -> config.anyRequest().permitAll());
-        http.oauth2Login(oauth2Configurer -> oauth2Configurer
-                .loginPage("/login") //로그인이 필요한데 로그인을 하지 않았다면 이동할 uri 설정
-                .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(oAuth2UserService))//로그인 완료 후 회원 정보 받기
-                .successHandler(successHandler()));
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(config -> config.anyRequest().permitAll())
+                .oauth2Login(oauth2Configurer -> oauth2Configurer
+                        .loginPage("/login") //로그인이 필요한데 로그인을 하지 않았다면 이동할 uri 설정
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(oAuth2UserService)) //로그인 완료 후 회원 정보 받기
+                        .successHandler(successHandler()));
 
         return http.build();
     }
 
     @Bean
     public AuthenticationSuccessHandler successHandler() {
+        log.info("successHandler 실행");
         return ((request, response, authentication) -> {
             DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
-
-            String id = defaultOAuth2User.getAttributes().get("id").toString();
-            String body = """
-                    {"id":"%s"}
-                    """.formatted(id);
-
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-
-            PrintWriter writer = response.getWriter();
-            writer.println(body);
-            writer.flush();
+//
+//            String id = defaultOAuth2User.getAttributes().get("id").toString();
+//            String body = """
+//                    {"id":"%s"}
+//                    """.formatted(id);
+//
+//            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+//            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+//
+//            PrintWriter writer = response.getWriter();
+//            writer.println(body);
+//            writer.flush();
         });
     }
 }
