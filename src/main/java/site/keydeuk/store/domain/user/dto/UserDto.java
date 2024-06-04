aa07974d0c45a47dad96be57f3ed1fd4f1b19d45
Su-daa <subin.kk019@gmail.com>
2024-06-04 19:56:55 +0900

    Feat: userDto 작성

--- src/main/java/site/keydeuk/store/domain/user/dto/UserDto.java
+++ src/main/java/site/keydeuk/store/domain/user/dto/UserDto.java
@@ -1,4 +1,21 @@
 package site.keydeuk.store.domain.user.dto;
 
-public class UserDto {
+import site.keydeuk.store.entity.enums.Gender;
+import site.keydeuk.store.entity.enums.RoleType;
+
+import java.time.LocalDate;
+
+public record UserDto(
+    Long id,
+    String email,
+    LocalDate birth,
+    String phone,
+    Gender gender,
+    RoleType role,
+    Boolean status,
+    String nickname,
+    String provider,
+    String providerId
+){
+//    public static UserDto of(){}
 }
