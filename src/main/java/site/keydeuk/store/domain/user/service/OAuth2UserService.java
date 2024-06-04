7ffc560d4f16151e44573cf709bf46873363369d
Su-daa <subin.kk019@gmail.com>
2024-06-04 16:35:32 +0900

    Feat: loadUser에서 PrincipalDetails 객체 반환

--- src/main/java/site/keydeuk/store/domain/user/service/OAuth2UserService.java
+++ src/main/java/site/keydeuk/store/domain/user/service/OAuth2UserService.java
@@ -1,33 +1,67 @@
 package site.keydeuk.store.domain.user.service;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import site.keydeuk.store.domain.security.PrincipalDetails;
+import site.keydeuk.store.domain.user.dto.OAuth2UserInfo;
+import site.keydeuk.store.domain.user.repository.UserRepository;
+import site.keydeuk.store.entity.User;
 
-import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class OAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-
-        // Role generate
-        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_ADMIN");
-
-        // nameAttributeKey
+        // 유저 정보(attributes) 가져오기
+        Map<String, Object> oAuth2UserAttributes = oAuth2User.getAttributes();
+        // resistrationId 가져오기 -> registrationId = 어떤 소셜 로그인 이용했는지
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        //userNameAttributeName 가져오기
         String userNameAttributeName = userRequest.getClientRegistration()
                 .getProviderDetails()
                 .getUserInfoEndpoint()
                 .getUserNameAttributeName();
 
-        // DB 저장로직이 필요하면 추가
+        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfo.of(registrationId, oAuth2UserAttributes);
 
-        return new DefaultOAuth2User(authorities, oAuth2User.getAttributes(), userNameAttributeName);
+        //TODO: 회원가입
+        User user = getOrSave(oAuth2UserInfo);
+        if (user.getNickname() == null) {
+
+        }
+
+        return new PrincipalDetails(user, oAuth2UserAttributes, userNameAttributeName);
+    }
+
+    private User getOrSave(OAuth2UserInfo oAuth2UserInfo) {
+        Optional<User> user = userRepository.findByEmail(oAuth2UserInfo.email());
+
+        if (user.isEmpty()) {
+            User newUser = User.builder()
+                    .email(oAuth2UserInfo.email())
+                    .imgUrl(oAuth2UserInfo.profile())
+                    .nickname(oAuth2UserInfo.name())
+                    .build();
+            log.info("{}", newUser);
+            if (newUser.getNickname() == null) {
+
+            }
+            return userRepository.save(newUser);
+        } else {
+            return user.get();
+        }
     }
 }
