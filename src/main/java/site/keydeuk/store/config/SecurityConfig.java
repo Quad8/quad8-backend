bd71818d3bec0c54ca435641ab17c3f8ca1691de
Su-daa <subin.kk019@gmail.com>
2024-06-04 19:59:25 +0900

    Feat: cors 설정 수정

--- src/main/java/site/keydeuk/store/config/SecurityConfig.java
+++ src/main/java/site/keydeuk/store/config/SecurityConfig.java
@@ -8,18 +8,44 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import site.keydeuk.store.domain.user.service.OAuth2UserService;
+
+import java.util.List;
+
+import static org.springframework.http.HttpMethod.*;
 
 @Slf4j
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private static final String[] PERMIT_ALL_URLS = new String[]{
+            "/dummy/**",
+            "/test/**",
+            "/payments/**",
+            "/home",
+    };
+
+    private static final String[] PERMIT_ALL_GET_URLS = new String[]{
+            "/favicon.ico",
+            "/docs/**",
+            "/products",
+    };
+
+    private static final String[] PERMIT_ALL_POST_URLS = new String[]{
+            "/users",
+    };
+
     private final OAuth2UserService oAuth2UserService;
 
     @Bean
@@ -35,7 +61,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("/localhost:8080/**")
+                        .allowedOrigins("/localhost:8080/")
                         .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                         .allowCredentials(true)
@@ -47,6 +73,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(createCorsConfigurationSource()))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PERMIT_ALL_URLS).permitAll()
+                        .requestMatchers(GET, PERMIT_ALL_GET_URLS).permitAll()
+                        .requestMatchers(POST, PERMIT_ALL_POST_URLS).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .authorizeHttpRequests(config -> config.anyRequest().permitAll())
                 .oauth2Login(oauth2Configurer -> oauth2Configurer
                         .loginPage("/login") //로그인이 필요한데 로그인을 하지 않았다면 이동할 uri 설정
@@ -74,6 +110,29 @@ public class SecurityConfig {
 //            writer.println(body);
 //            writer.flush();
         });
+    }
+
+    @Bean
+    public CorsConfigurationSource createCorsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of(
+                "http://localhost:3000" //프론트 주소
+        ));
+        corsConfiguration.setAllowedMethods(List.of(
+                GET.name(),
+                POST.name(),
+                PUT.name(),
+                PATCH.name(),
+                DELETE.name(),
+                OPTIONS.name()
+        ));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
 
