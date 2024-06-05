d281f8586d5c4686ebb4a04eb1943133275d167c
Su-daa <subin.kk019@gmail.com>
2024-06-06 01:11:38 +0900

    Feat: email로 유저 조회 메서드 추가

--- src/main/java/site/keydeuk/store/domain/user/repository/UserRepository.java
+++ src/main/java/site/keydeuk/store/domain/user/repository/UserRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByProviderAndProviderId(String provider, String providerId);
-    boolean existsByNickname(String nickname);
+    Optional<User> findByEmail(String email);
 
+    boolean existsByNickname(String nickname);
 }
