2c6aa5f6ac8fe78d1f4ae083a98be2ef4ba29c36
Su-daa <subin.kk019@gmail.com>
2024-06-06 01:13:37 +0900

    Feat: 카카오로부터 받은 정보로 유저 정보 만드는 클래스 생성

---
+++ src/main/java/site/keydeuk/store/domain/user/dto/KakaoOAuth2UserInfo.java
@@ -0,0 +1,45 @@
+package site.keydeuk.store.domain.user.dto;
+
+import site.keydeuk.store.entity.enums.ProviderType;
+
+import java.util.Map;
+
+import static site.keydeuk.store.entity.enums.ProviderType.*;
+
+public class KakaoOAuth2UserInfo implements OAuth2UserInfo {
+    private final String providerId;
+    private final Map<String, Object> properties;
+    private final Map<String, Object> kakaoAccount;
+
+    public KakaoOAuth2UserInfo(Map<String, Object> oAuth2UserAttributes) {
+        this.providerId = String.valueOf(oAuth2UserAttributes.get("id"));
+        this.properties = (Map<String, Object>) oAuth2UserAttributes.get("properties");
+        this.kakaoAccount = (Map<String, Object>) oAuth2UserAttributes.get("kakao_account");
+    }
+
+
+    @Override
+    public String getNickname() {
+        return String.valueOf(properties.get("nickname"));
+    }
+
+    @Override
+    public String getEmail() {
+        return String.valueOf(kakaoAccount.get("email"));
+    }
+
+    @Override
+    public String getProfileImgUrl() {
+        return String.valueOf(properties.get("profile_image"));
+    }
+
+    @Override
+    public ProviderType getProvider() {
+        return KAKAO;
+    }
+
+    @Override
+    public String getProviderId() {
+        return providerId;
+    }
+}
