0b426533a0a599255865b02837903701f2947dd2
Su-daa <subin.kk019@gmail.com>
2024-06-06 00:55:04 +0900

    Feat: 소셜 로그인별 유저 정보 가져오는 인터페이스 구현 - 소셜 별 유저 정보를 가져 오기 위하여 인터페이스로 구현 - 소셜 별 정보를 주는 방법이 달라서 고안한 방법

--- src/main/java/site/keydeuk/store/domain/user/service/OAuth2UserService.java
+++ src/main/java/site/keydeuk/store/domain/user/service/OAuth2UserService.java
@@ -8,12 +8,15 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import site.keydeuk.store.domain.security.PrincipalDetails;
+import site.keydeuk.store.domain.user.dto.OAuth2Attributes;
 import site.keydeuk.store.domain.user.dto.OAuth2UserInfo;
 import site.keydeuk.store.domain.user.repository.UserRepository;
+import site.keydeuk.store.domain.user.utils.OAuth2Utils;
 import site.keydeuk.store.entity.User;
+import site.keydeuk.store.entity.enums.RoleType;
+import site.keydeuk.store.entity.enums.ProviderType;
 
 import java.util.Map;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,8 +28,7 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        // 유저 정보(attributes) 가져오기
-        Map<String, Object> oAuth2UserAttributes = oAuth2User.getAttributes();
+
         // resistrationId 가져오기 -> registrationId = 어떤 소셜 로그인 이용했는지
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         //userNameAttributeName 가져오기
@@ -34,34 +36,27 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
                 .getProviderDetails()
                 .getUserInfoEndpoint()
                 .getUserNameAttributeName();
+        ProviderType providerType = OAuth2Utils.getSocialType(registrationId);
 
-        OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfo.of(registrationId, oAuth2UserAttributes);
+        // 유저 정보(attributes) 가져오기
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        log.info("{}", attributes);
+        OAuth2Attributes oAuth2Attributes = OAuth2Attributes.of(providerType, userNameAttributeName, attributes);
+        OAuth2UserInfo oAuth2UserInfo = oAuth2Attributes.getOAuth2UserInfo();
 
         //TODO: 회원가입
-        User user = getOrSave(oAuth2UserInfo);
-        if (user.getNickname() == null) {
-
-        }
-
-        return new PrincipalDetails(user, oAuth2UserAttributes, userNameAttributeName);
+        User user = getUser(oAuth2UserInfo);
+        return new PrincipalDetails(user, attributes, userNameAttributeName);
     }
 
-    private User getOrSave(OAuth2UserInfo oAuth2UserInfo) {
-        Optional<User> user = userRepository.findByEmail(oAuth2UserInfo.email());
-
-        if (user.isEmpty()) {
-            User newUser = User.builder()
-                    .email(oAuth2UserInfo.email())
-                    .imgUrl(oAuth2UserInfo.profile())
-                    .nickname(oAuth2UserInfo.name())
-                    .build();
-            log.info("{}", newUser);
-            if (newUser.getNickname() == null) {
-
-            }
-            return userRepository.save(newUser);
-        } else {
-            return user.get();
-        }
+    private User getUser(OAuth2UserInfo oAuth2UserInfo) {
+        String provider = oAuth2UserInfo.getProvider().name();
+        return userRepository.findByProviderAndProviderId(provider, oAuth2UserInfo.getProviderId())
+                .orElseGet(()-> User.builder()
+                        .email(oAuth2UserInfo.getEmail())
+                        .imgUrl(oAuth2UserInfo.getProfileImgUrl())
+                        .nickname(oAuth2UserInfo.getNickname())
+                        .role(RoleType.ROLE_GUEST)
+                        .build());
     }
 }
--- src/main/java/site/keydeuk/store/domain/user/utils/OAuth2Utils.java
+++ src/main/java/site/keydeuk/store/domain/user/utils/OAuth2Utils.java
@@ -1,25 +1,24 @@
 package site.keydeuk.store.domain.user.utils;
 
-import site.keydeuk.store.entity.enums.SocialType;
+import site.keydeuk.store.domain.user.dto.OAuth2UserInfo;
+import site.keydeuk.store.entity.enums.ProviderType;
+
+import java.util.Map;
 
 /**
  * 소셜 타입 분기처리 진행
  */
 public class OAuth2Utils {
     // registrationID 를 보고 어떤 소셜에서 인증을 했는지 반환
-    public static SocialType getSocialType(String registrationId) {
+    public static ProviderType getSocialType(String registrationId) {
         // Member 에서도 사용하기 위해 대문자로 변경
         if (registrationId != null) {
             registrationId = registrationId.toUpperCase();
         }
-
-        if ("GOOGLE".equals(registrationId)) {
-            return SocialType.GOOGLE;
-        } else if ("KAKAO".equals(registrationId)) {
-            return SocialType.KAKAO;
-        } else if ("GITHUB".equals(registrationId)) {
-            return SocialType.GITHUB;
-        }
-        return null;
+        return ProviderType.valueOf(registrationId);
+    }
+    // 소셜로부터 받은 정보들을 받아 파싱하는 OAuth2UserInfo 를 생성하는 메서드
+    public static OAuth2UserInfo getOAuth2UserInfo(ProviderType providerType, Map<String, Object> attributes) {
+        return providerType.oAuth2UserInfoFrom(attributes);
     }
 }
