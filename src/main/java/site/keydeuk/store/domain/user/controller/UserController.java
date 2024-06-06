9d8c99faa752ac5e5673d04dd43b9ea3f95eb772
Su-daa <subin.kk019@gmail.com>
2024-06-07 00:27:18 +0900

    Feat: mapping 주소 변경

--- src/main/java/site/keydeuk/store/domain/user/controller/UserController.java
+++ src/main/java/site/keydeuk/store/domain/user/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.keydeuk.store.common.response.CommonResponse;
 import site.keydeuk.store.domain.user.dto.JoinRequest;
@@ -11,10 +12,11 @@ import site.keydeuk.store.domain.user.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
-    @PostMapping
+    @PostMapping("/join")
     public CommonResponse<Long> join(@RequestBody @Validated JoinRequest joinRequest) {
         Long userId = userService.join(joinRequest);
         return CommonResponse.ok(userId);
