20582281418bb5b61e7917e941f95ebedb2b09e2
seoseo17 <eyyss16@gmail.com>
2024-06-07 02:04:09 +0900

    Feature: 목록 조회 페이징 처리

--- src/main/java/site/keydeuk/store/domain/product/dto/productlist/ProductListAndTotalResponseDto.java
+++ src/main/java/site/keydeuk/store/domain/product/dto/productlist/ProductListAndTotalResponseDto.java
@@ -3,6 +3,7 @@ package site.keydeuk.store.domain.product.dto.productlist;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -11,8 +12,8 @@ import java.util.List;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class ProductListAndTotalResponseDto {
 
-    private int totalCount;
+    private long totalCount;
 
-    private List<ProductListResponseDto> list;
+    private Page<ProductListResponseDto> list;
 
 }
--- src/main/java/site/keydeuk/store/domain/product/repository/ProductRepository.java
+++ src/main/java/site/keydeuk/store/domain/product/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package site.keydeuk.store.domain.product.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -13,9 +15,10 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 
-    List<Product> findByProductCategoryId(Integer categoryId);
+    Page<Product> findByProductCategoryId(Integer categoryId,Pageable pageable);
+
     @Query("select p from Product p where p.productCategory.id between :startCategoryId and :endCategoryId")
-    List<Product> findProductsByCategoryIdBetween(
-            @Param("startCategoryId") Integer startId, @Param("endCategoryId") Integer endId);
+    Page<Product> findProductsByCategoryIdBetween(
+            @Param("startCategoryId") Integer startId, @Param("endCategoryId") Integer endId, Pageable pageable);
 
 }
