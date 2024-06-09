b4530e54142df33c55ba019aae668442b436d6c5
Su-daa <subin.kk019@gmail.com>
2024-06-09 09:52:50 +0900

    Feat: enum, localdate 타입 검증 notNull로 변경

--- src/main/java/site/keydeuk/store/domain/user/dto/request/JoinRequest.java
+++ src/main/java/site/keydeuk/store/domain/user/dto/request/JoinRequest.java
@@ -3,6 +3,7 @@ package site.keydeuk.store.domain.user.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import site.keydeuk.store.entity.User;
 import site.keydeuk.store.entity.enums.Gender;
@@ -17,12 +18,12 @@ public record JoinRequest(
         @NotEmpty(message = "비밀번호는 필수 입력 항목입니다.")
         @Size(min = 6, max = 20, message = "비밀번호는 6자 이상, 20자 이하여야 합니다.")
         String password,
-        @NotEmpty
+        @NotNull
         LocalDate birth,
         @NotEmpty(message = "휴대폰 번호는 필수 입력 항목입니다.")
         @Size(min = 11, max = 11, message = "휴대폰 번호는 11자의 숫자여야 합니다.")
         String phone,
-        @NotEmpty
+        @NotNull
         Gender gender,
         @NotEmpty(message = "닉네임은 필수 입력 항목입니다.")
         @Size(max = 15, message = "닉네임은 1글자 이상, 15자 이하여야 합니다.")
