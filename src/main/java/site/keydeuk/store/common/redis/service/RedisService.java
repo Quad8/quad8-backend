c6aea86362c5855b72d26c5d8efb6e4b5a1f555f
Su-daa <subin.kk019@gmail.com>
2024-06-10 16:52:47 +0900

    Feat: redis 안쓰는 메서드 삭제

--- src/main/java/site/keydeuk/store/common/redis/service/RedisService.java
+++ src/main/java/site/keydeuk/store/common/redis/service/RedisService.java
@@ -15,20 +15,4 @@ public interface RedisService {
     boolean setIfAbsent(String key, Object value, Long expiredTime);
 
     boolean delete(String key);
-
-    <T> List<T> getList(String key, Class<T> type);
-
-    void rightPop(String key);
-
-    void leftPush(String key, Object value);
-
-    void removeList(String key, Object value);
-
-    Double getZScore(String key, String keyword);
-
-    void addZScore(String key, String keyword, Double score);
-
-    Set<ZSetOperations.TypedTuple<String>> getZRank(String key, long start, long end);
-
-    Set<String> getZRangeByLex(String key, String prefix, int limit);
 }
