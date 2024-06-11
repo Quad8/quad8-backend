4b4089a96b4b8f6233a4a24c271379ed68d42c2c
Su-daa <subin.kk019@gmail.com>
2024-06-11 17:46:54 +0900

    Feat: 핸드폰 번호로 유저 조회

--- src/main/java/site/keydeuk/store/domain/user/repository/UserRepository.java
+++ src/main/java/site/keydeuk/store/domain/user/repository/UserRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByProviderAndProviderId(String provider, String providerId);
     Optional<User> findByEmail(String email);
+    Optional<User> findByPhone(String phone);
 
     boolean existsByNickname(String nickname);
 }
