abbf841de094c374bcc28647a23d60f0bc71b7ae
Su-daa <subin.kk019@gmail.com>
2024-06-12 22:47:31 +0900

    Feat: 배송지 관련 controller 생성

--- src/main/java/site/keydeuk/store/domain/shipping/controller/ShippingController.java
+++ src/main/java/site/keydeuk/store/domain/shipping/controller/ShippingController.java
@@ -1,4 +1,12 @@
 package site.keydeuk.store.domain.shipping.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/shipping")
+@RequiredArgsConstructor
 public class ShippingController {
+
 }
