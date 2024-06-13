7ffde36819049339eee23465ebb9d1d295f774fb
Su-daa <subin.kk019@gmail.com>
2024-06-14 00:29:12 +0900

    Feat: 기본 배송지 설정시 기존 기본 배송지 해제

--- src/main/java/site/keydeuk/store/domain/shipping/service/ShippingService.java
+++ src/main/java/site/keydeuk/store/domain/shipping/service/ShippingService.java
@@ -2,6 +2,7 @@ package site.keydeuk.store.domain.shipping.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import site.keydeuk.store.domain.security.PrincipalDetails;
 import site.keydeuk.store.domain.shipping.dto.request.SaveShippingAddressRequest;
 import site.keydeuk.store.domain.shipping.dto.response.ShippingAddressResponse;
@@ -14,8 +15,13 @@ import site.keydeuk.store.entity.User;
 public class ShippingService {
     private final ShippingRepository shippingRepository;
 
+    @Transactional
     public ShippingAddressResponse saveShippingAddress(PrincipalDetails principalDetails, SaveShippingAddressRequest shippingAddressRequest) {
         User user = principalDetails.getUser();
+
+        if (shippingAddressRequest.isDefault()) {
+            shippingRepository.updateDefaultAddressToFalse(user.getId());
+        }
         ShippingAddress shippingAddress = shippingAddressRequest.toEntity(user, shippingAddressRequest);
         ShippingAddress savedShippingAddress = shippingRepository.save(shippingAddress);
         return ShippingAddressResponse.from(savedShippingAddress);
