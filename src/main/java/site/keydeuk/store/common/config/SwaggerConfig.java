12779a9645adc70749099557611cd227a94285b3
Su-daa <subin.kk019@gmail.com>
2024-06-13 19:11:48 +0900

    Feat: swagger Authorize 버튼 활성화

--- src/main/java/site/keydeuk/store/common/config/SwaggerConfig.java
+++ src/main/java/site/keydeuk/store/common/config/SwaggerConfig.java
@@ -2,10 +2,16 @@ package site.keydeuk.store.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @OpenAPIDefinition(info = @Info(title = "keyDeuk API 명세서",description = "Kedeuk API 명세서",version = "v1"))
 @RequiredArgsConstructor
@@ -21,4 +27,17 @@ public class SwaggerConfig {
                 .pathsToMatch(paths)
                 .build();
     }
+
+    @Bean
+    public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
+
 }
