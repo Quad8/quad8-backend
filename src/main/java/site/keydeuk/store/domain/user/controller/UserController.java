8ab15de149483662bfb14962458f959d2e895460
Su-daa <subin.kk019@gmail.com>
2024-06-14 11:00:37 +0900

    Docs: 배송지, 유저 관련 스웨거 수정

--- src/main/java/site/keydeuk/store/domain/user/controller/UserController.java
+++ src/main/java/site/keydeuk/store/domain/user/controller/UserController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -26,22 +27,24 @@ public class UserController {
     @Operation(summary = "회원가입", description = "회원가입을 위한 api 입니다.")
     @PostMapping
     public CommonResponse<Long> join(
-            @Validated @RequestPart("joinRequest") JoinRequest joinRequest,
-            @RequestPart("imgFile") MultipartFile imgFile) {
+            @Validated @RequestPart("joinRequest") @ParameterObject JoinRequest joinRequest,
+            @RequestPart("imgFile") @ParameterObject MultipartFile imgFile) {
         Long userId = userService.join(joinRequest, imgFile);
         return CommonResponse.ok(userId);
     }
 
     @Operation(summary = "이메일 중복 확인", description = "주어진 이메일이 시스템에 이미 존재하는지 확인합니다.")
     @GetMapping("/check/email")
-    public CommonResponse<Boolean> duplicateEmail(@Parameter(description = "확인할 이메일 주소", example = "user@example.com") @RequestParam String email) {
+    public CommonResponse<Boolean> duplicateEmail(
+            @Parameter(description = "확인할 이메일 주소", example = "user@example.com") @RequestParam String email) {
         boolean response = userService.isExistEmail(email);
         return CommonResponse.ok(response);
     }
 
     @Operation(summary = "닉네임 중복 확인", description = "주어진 닉네임이 시스템에 이미 존재하는지 확인합니다.")
     @GetMapping("/check/nickname")
-    public CommonResponse<Boolean> duplicateNickname(@Parameter(description = "확인할 닉네임", example = "nickname123") @RequestParam String nickname) {
+    public CommonResponse<Boolean> duplicateNickname(
+            @Parameter(description = "확인할 닉네임", example = "nickname123") @RequestParam String nickname) {
         boolean response = userService.isExistNickname(nickname);
         return CommonResponse.ok(response);
     }
@@ -58,7 +61,7 @@ public class UserController {
     @PutMapping("/me")
     public CommonResponse<Void> updateProfile(
             @AuthenticationPrincipal PrincipalDetails principalDetails,
-            @RequestBody @Validated UpdateProfileRequest updateProfileRequest
+            @RequestBody @Validated @ParameterObject UpdateProfileRequest updateProfileRequest
     ) {
         Long userId = principalDetails.getUserId();
         userService.updateProfile(userId, updateProfileRequest);
