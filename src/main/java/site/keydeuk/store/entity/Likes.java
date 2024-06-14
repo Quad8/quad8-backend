7d5705d25f8cd55b75759a167c7ed65e88ede94a
Su-daa <subin.kk019@gmail.com>
2024-06-15 03:18:41 +0900

    Feat: 좋아요 엔티티

--- src/main/java/site/keydeuk/store/entity/Likes.java
+++ src/main/java/site/keydeuk/store/entity/Likes.java
@@ -1,4 +1,28 @@
 package site.keydeuk.store.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "likes")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "productId", nullable = false)
+    private Product product;
 }
