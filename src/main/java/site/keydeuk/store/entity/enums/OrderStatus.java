18a26ce6adfa274ed5c90da6470343946d6b931d
Su-daa <subin.kk019@gmail.com>
2024-06-17 20:18:57 +0900

    Feat: 주문 상태 작성

---
+++ src/main/java/site/keydeuk/store/entity/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package site.keydeuk.store.entity.enums;
+
+public enum OrderStatus {
+    PAYMENT_COMPLETED, // 결제 완료
+    PREPARING,         // 배송 준비 중
+    SHIPPING,          // 배송 중
+    DELIVERED,         // 배송 완료
+    CANCELED           // 주문 취소
+}
