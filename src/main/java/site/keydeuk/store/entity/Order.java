16f15de9a2676cc148b5c6562c99962db63b4690
Su-daa <subin.kk019@gmail.com>
2024-06-17 20:18:42 +0900

    Feat: 주문 엔티티 작성

--- src/main/java/site/keydeuk/store/entity/Order.java
+++ src/main/java/site/keydeuk/store/entity/Order.java
@@ -1,4 +1,25 @@
 package site.keydeuk.store.entity;
 
-public class Order {
+import jakarta.persistence.*;
+import lombok.*;
+import site.keydeuk.store.common.entity.BaseTimeEntity;
+import site.keydeuk.store.entity.enums.OrderStatus;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Order extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private boolean isCustom;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+    private Long totalPrice;
+    @OneToOne
+    private Payment payment;
 }
