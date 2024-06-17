1b4a2719d47128eef464269343d68171f4670369
Su-daa <subin.kk019@gmail.com>
2024-06-18 01:55:10 +0900

    Feat: 주문 아이템 엔티티

---
+++ src/main/java/site/keydeuk/store/entity/OrderItem.java
@@ -0,0 +1,21 @@
+package site.keydeuk.store.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private Integer productId;
+    private Long switchOptionId;
+    private Integer count;
+}
