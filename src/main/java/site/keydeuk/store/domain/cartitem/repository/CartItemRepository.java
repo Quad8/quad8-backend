69b7b64e02769b7969b558da253ca2363b3b0537
seoseo17 <eyyss16@gmail.com>
2024-06-17 23:41:20 +0900

    feature: 장바구니 삭제

--- src/main/java/site/keydeuk/store/domain/cartitem/repository/CartItemRepository.java
+++ src/main/java/site/keydeuk/store/domain/cartitem/repository/CartItemRepository.java
@@ -1,5 +1,6 @@
 package site.keydeuk.store.domain.cartitem.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import site.keydeuk.store.entity.CartItem;
@@ -11,5 +12,7 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItemWithProduct findByCartIdAndProductId(Long cartId, Integer productId);
     CartItemWithProduct findByCartIdAndProductIdAndOptionId(Long cartId, Integer productId,Long OptionId);
     CartItemWithCustom findByCartIdAndCustomOptionId(Long cartId, Integer customOptionId);
+    @Transactional
+    void deleteByIdAndCartId(Long cartItemId, Long cartId);
 
 }
