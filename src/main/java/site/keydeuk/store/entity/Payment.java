3337f17d62695569a785af8f0b2bc2ad19adb5eb
Su-daa <subin.kk019@gmail.com>
2024-06-19 13:17:00 +0900

    Feat: payment가 orderId 알도록

--- src/main/java/site/keydeuk/store/entity/Payment.java
+++ src/main/java/site/keydeuk/store/entity/Payment.java
@@ -1,13 +1,14 @@
 package site.keydeuk.store.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import site.keydeuk.store.common.entity.BaseTimeEntity;
+import jakarta.persistence.OneToOne;
 
 import java.time.LocalDateTime;
 
 @Entity
-public class Payment extends BaseTimeEntity {
+public class Payment {
     @Id
     Long id;
     String paymentKey;
@@ -17,5 +18,7 @@ public class Payment extends BaseTimeEntity {
     LocalDateTime requestedAt;
     LocalDateTime approvedAt;
     String lastTransactionKey;
+    @OneToOne(fetch = FetchType.LAZY)
+    private Order order;
 
 }
