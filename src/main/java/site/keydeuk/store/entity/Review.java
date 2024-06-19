4e9b49b705ce500ab3c18d4f7664b51098fc7e07
Su-daa <subin.kk019@gmail.com>
2024-06-19 12:50:46 +0900

    Feat: 리뷰 엔티티 수정

--- src/main/java/site/keydeuk/store/entity/Review.java
+++ src/main/java/site/keydeuk/store/entity/Review.java
@@ -1,4 +1,45 @@
 package site.keydeuk.store.entity;
 
-public class Review {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.keydeuk.store.common.entity.BaseTimeEntity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "review")
+public class Review extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private Double score;
+    private Long viewCount;
+    private Integer option1;
+    private Integer option2;
+    private Integer option3;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "reviewId")
+    private List<ReviewImg> reviewImages = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "productId")
+    private Product product;
+
+    public void addReviewImgs(Collection<ReviewImg> reviewImgs) {
+        this.reviewImages.addAll(reviewImgs);
+    }
 }
