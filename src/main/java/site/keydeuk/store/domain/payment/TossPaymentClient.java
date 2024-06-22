76c08f413125ff6c2cc82c3733fc1d769ca441d3
Su-daa <subin.kk019@gmail.com>
2024-06-22 17:42:43 +0900

    Feat: 토스 시크릿 키 환경변수 등록

--- src/main/java/site/keydeuk/store/domain/payment/TossPaymentClient.java
+++ src/main/java/site/keydeuk/store/domain/payment/TossPaymentClient.java
@@ -1,7 +1,7 @@
 package site.keydeuk.store.domain.payment;
 
-import lombok.RequiredArgsConstructor;
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import site.keydeuk.store.domain.payment.dto.PaymentConfirmRequest;
@@ -9,21 +9,24 @@ import site.keydeuk.store.domain.payment.dto.PaymentConfirmResponse;
 
 
 @Component
-@RequiredArgsConstructor
 public class TossPaymentClient implements PaymentClient {
 
     private static final String DOMAIN = "https://api.tosspayments.com/v1/payments";
     private static final String CONFIRM_PATH = "/confirm";
-    //    private static final String SECRET_KEY = new String(Base64.encodeBase64("test_sk_6BYq7GWPVvy5eRMWwjam3NE5vbo1:".getBytes()));
-        private static final String SECRET_KEY = new String(Base64.encodeBase64("test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6:".getBytes()));
-
     private final RestClient restClient;
+    private final String secretKey;
+
+    public TossPaymentClient(RestClient restClient,
+                             @Value("${toss.secret-key}") String secretKey) {
+        this.restClient = restClient;
+        this.secretKey = new String(Base64.encodeBase64(secretKey.getBytes()));
+    }
 
     @Override
     public PaymentConfirmResponse confirm(PaymentConfirmRequest request) {
         return restClient.post()
                 .uri(DOMAIN + CONFIRM_PATH)
-                .header("Authorization", "Basic "+SECRET_KEY)
+                .header("Authorization", "Basic " + secretKey)
                 .body(request)
                 .retrieve()
                 .body(PaymentConfirmResponse.class);
