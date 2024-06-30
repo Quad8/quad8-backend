0011014d4a66d3adf28aabdfbdec684a05a5e6b0
Su-daa <subin.kk019@gmail.com>
2024-07-01 00:51:39 +0900

    Feat: 사용자 찜 목록 페이지네이션 구현

--- src/main/java/site/keydeuk/store/domain/likes/repository/LikesRepository.java
+++ src/main/java/site/keydeuk/store/domain/likes/repository/LikesRepository.java
@@ -1,13 +1,14 @@
 package site.keydeuk.store.domain.likes.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.keydeuk.store.entity.Likes;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
-    List<Likes> findByUserId(Long userId);
+    Page<Likes> findByUserId(Long userId, Pageable pageable);
     Optional<Likes> findByUserIdAndProductId(Long userId, Integer productId);
     boolean existsByUserIdAndProductId(Long userId, Integer productId);
 }
