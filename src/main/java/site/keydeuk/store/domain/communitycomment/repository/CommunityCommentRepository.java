b5dac2c7d6f01759f4933ea2b990725bd7025bd5
seoseo17 <eyyss16@gmail.com>
2024-07-04 18:21:27 +0900

    feat: 커뮤니티 댓글 무한스크롤 적용:

--- src/main/java/site/keydeuk/store/domain/communitycomment/repository/CommunityCommentRepository.java
+++ src/main/java/site/keydeuk/store/domain/communitycomment/repository/CommunityCommentRepository.java
@@ -1,6 +1,10 @@
 package site.keydeuk.store.domain.communitycomment.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import site.keydeuk.store.entity.CommunityComment;
 
@@ -9,10 +13,13 @@ import java.util.List;
 @Repository
 public interface CommunityCommentRepository extends JpaRepository<CommunityComment,Long> {
 
-    List<CommunityComment> findByCommunity_Id(Long communityId);
+    List<CommunityComment> findByCommunity_Id(Long communityId, Pageable pageable);
 
     Long countByCommunity_Id(Long communityId);
 
     void deleteByCommunity_Id(Long id);
+
+    @Query(value = "select c from CommunityComment c where c.id > ?1")
+    Page<CommunityComment> findByCommunityCommentIdLessThan(Long commentId, PageRequest pageRequest);
 
 }
