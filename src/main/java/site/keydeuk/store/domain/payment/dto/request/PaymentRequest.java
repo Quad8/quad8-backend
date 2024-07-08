804549e2db6ad7b4335a47b6f04854fafcd2dcfa
Su-daa <subin.kk019@gmail.com>
2024-07-08 11:46:57 +0900

    Feat: 결제 승인, 결제 성공 요청 객체에 orderId 추가

--- src/main/java/site/keydeuk/store/domain/payment/dto/request/PaymentRequest.java
+++ src/main/java/site/keydeuk/store/domain/payment/dto/request/PaymentRequest.java
@@ -1,6 +1,7 @@
 package site.keydeuk.store.domain.payment.dto.request;
 
 public record PaymentRequest (
+        Long orderId,
         String paymentKey,
         String paymentOrderId,
         Long amount
