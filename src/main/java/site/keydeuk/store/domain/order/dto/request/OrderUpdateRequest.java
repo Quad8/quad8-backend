5cc77c1a253c3b9453382c0a70205ec0dd6a5409
Su-daa <subin.kk019@gmail.com>
2024-07-08 11:44:33 +0900

    Feat: 주문 수정시 요청에서 결제 주문 아이디 제외

--- src/main/java/site/keydeuk/store/domain/order/dto/request/OrderUpdateRequest.java
+++ src/main/java/site/keydeuk/store/domain/order/dto/request/OrderUpdateRequest.java
@@ -1,7 +1,6 @@
 package site.keydeuk.store.domain.order.dto.request;
 
 public record OrderUpdateRequest(
-        String paymentOrderId,
         Long shippingAddressId,
         String deliveryMessage
 ) {
