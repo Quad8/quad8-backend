f78b9e4aebfafdca6c2f20e4506cb44b4f7da2c9
Su-daa <subin.kk019@gmail.com>
2024-07-14 23:04:03 +0900

    Feat: 쿠폰 생성 기능 추가

--- src/main/java/site/keydeuk/store/domain/coupon/service/CouponService.java
+++ src/main/java/site/keydeuk/store/domain/coupon/service/CouponService.java
@@ -1,4 +1,52 @@
 package site.keydeuk.store.domain.coupon.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.keydeuk.store.common.exception.CustomException;
+import site.keydeuk.store.domain.coupon.dto.request.CreateCouponRequest;
+import site.keydeuk.store.domain.coupon.dto.response.CouponResponse;
+import site.keydeuk.store.domain.coupon.repository.CouponRepository;
+import site.keydeuk.store.domain.coupon.repository.UserCouponRepository;
+import site.keydeuk.store.domain.user.repository.UserRepository;
+import site.keydeuk.store.entity.Coupon;
+import site.keydeuk.store.entity.User;
+import site.keydeuk.store.entity.UserCoupon;
+
+import static site.keydeuk.store.common.response.ErrorCode.USER_NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
 public class CouponService {
+    private final UserCouponRepository userCouponRepository;
+    private final CouponRepository couponRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public CouponResponse createCoupon(Long userId, CreateCouponRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+        Coupon coupon = Coupon.builder()
+                .name(request.name())
+                .price(request.price())
+                .minPrice(request.minPrice())
+                .expiredAt(request.expiredDate())
+                .build();
+        Coupon savedCoupon = couponRepository.save(coupon);
+        UserCoupon userCoupon = UserCoupon.builder()
+                .user(user)
+                .coupon(savedCoupon)
+                .build();
+
+        userCouponRepository.save(userCoupon);
+
+        return CouponResponse.builder()
+                .id(savedCoupon.getId())
+                .name(savedCoupon.getName())
+                .price(savedCoupon.getPrice())
+                .minPrice(savedCoupon.getMinPrice())
+                .expiredAt(savedCoupon.getExpiredAt())
+                .build();
+    }
 }
---
+++ src/main/java/site/keydeuk/store/entity/UserCoupon.java
@@ -0,0 +1,27 @@
+package site.keydeuk.store.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_coupon")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCoupon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "couponId", nullable = false)
+    private Coupon coupon;
+}
