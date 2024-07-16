0f63228343666193d163d1db9b4e6b66cb027a8e
Su-daa <subin.kk019@gmail.com>
2024-07-16 11:50:12 +0900

    Feat: 회원가입 쿠폰 여부 추가

--- src/main/java/site/keydeuk/store/domain/coupon/repository/UserCouponRepository.java
+++ src/main/java/site/keydeuk/store/domain/coupon/repository/UserCouponRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import site.keydeuk.store.entity.UserCoupon;
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
+    boolean existsByUserIdAndCouponIsWelcomeTrue(Long userId);
 }
