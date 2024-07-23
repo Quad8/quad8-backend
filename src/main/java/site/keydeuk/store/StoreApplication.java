68986ce46f591e76bece5a6be50775ec9c42d8fb
Su-daa <subin.kk019@gmail.com>
2024-07-24 00:01:54 +0900

    Chore: 안쓰는 import문 삭제

--- src/main/java/site/keydeuk/store/StoreApplication.java
+++ src/main/java/site/keydeuk/store/StoreApplication.java
@@ -1,12 +1,8 @@
 package site.keydeuk.store;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
