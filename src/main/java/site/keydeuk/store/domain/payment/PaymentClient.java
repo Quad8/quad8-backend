0cc2be37b8a88bd6f019e56a12d036800f763da1
Su-daa <subin.kk019@gmail.com>
2024-07-29 20:40:08 +0900

    Feat: 결제 승인 요청시 응답값 로그 추가

--- src/main/java/site/keydeuk/store/domain/payment/PaymentClient.java
+++ src/main/java/site/keydeuk/store/domain/payment/PaymentClient.java
@@ -1,9 +1,10 @@
 package site.keydeuk.store.domain.payment;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import site.keydeuk.store.domain.payment.dto.request.PaymentConfirmRequest;
 import site.keydeuk.store.domain.payment.dto.response.PaymentConfirmResponse;
 
 public interface PaymentClient {
 
-    PaymentConfirmResponse confirm(PaymentConfirmRequest request);
+    PaymentConfirmResponse confirm(PaymentConfirmRequest request) throws JsonProcessingException;
 }
